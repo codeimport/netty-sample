30719fc6b729e816adae15b071c457df1b1bcb0e
snake <gang47583322@aliyun.com>
2019-06-04 16:02:47 +0800

    init client server modual

---
+++ netty-server/src/main/java/com/github/codeimport/nettyserver/NettyServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.codeimport.nettyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyServerApplication.class, args);
+    }
+
+}
