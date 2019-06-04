b215aa3c80368deaccf4b1a504484e9a8a01e0a5
snake <gang47583322@aliyun.com>
2019-06-04 15:14:27 +0800

    first commit

---
+++ src/main/java/com/github/codeimport/nettysample/NettySampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.codeimport.nettysample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettySampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettySampleApplication.class, args);
+    }
+
+}
